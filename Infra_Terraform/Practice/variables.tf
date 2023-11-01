96ff28e02df8d95817d094ee1e18557c5803068a
Cybertas <cybertas00@google.com>
2023-11-01 17:19:17 +1100

    added terraform files for practice

---
+++ Infra_Terraform/Practice/variables.tf
@@ -0,0 +1,5 @@
+variable "instance_from_var_file" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstanc_from_variables.tf"
+}
